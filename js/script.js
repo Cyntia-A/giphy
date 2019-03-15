1be1ebe4df2bd24aad016589d98216be6bb2a854
Cintia Arambula <cintiaarambula@ccpaedu.com>
2019-03-15 01:24:09 +0000

    update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
+    
+    var searchTerm = $('#search-term').val();
+    var request_Url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC"
+       
+    $.ajax({
+       url: request_Url,
+       method: "GET",
+       success: function(response){
+           
+ 
+           var Image = response.data[0].images.original.url;
+            $('.text-center').html('<img src="' + Image + '"/>');       
+           
+       },
+    });
+    
   
 });
 
